956ceb652cbb2d39db7211d0e99a02f0ec333ea1
elistersilver <yulechka_antonova_1991@list.ru>
2021-11-21 23:54:06 +0300

    homework 21/11/21

---
+++ e.js
@@ -0,0 +1,5 @@
+let v = 31;
+let a = 3;
+let c = 5;
+c = v / a;
+console.log(c);
---
+++ work.js
@@ -0,0 +1,14 @@
+let arr = [4, 316, 3, -11, 35, 7];
+let x = 2
+let arrLen = arr.length;
+
+for (let i = 0; i < arrLen; i++) {
+
+if(arr[i] % 2 == 0) {
+let x = arr[i];
+
+
+console.log(x);}
+}
+
+
