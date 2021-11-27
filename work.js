375b91ff2dcb794acd656c1d09a95ae6cbf16061
elistersilver <yulechka_antonova_1991@list.ru>
2021-11-27 20:48:28 +0300

    hgvoiwsahf

--- work.js
+++ work.js
@@ -1,11 +1,11 @@
-let arr = [4, 316, 3, -11, 35, 7];
-let x = 2
+let arr = [4, 316, 3, -11, 36, 7, 8, 11, 29, 3, 14];
+let x = 2;
 let arrLen = arr.length;
 
 for (let i = 0; i < arrLen; i++) {
 
 if(arr[i] % 2 == 0) {
-var arr2 = arr.splice(i, 1, 3);
+var arr2 = arr.splice(i, 1, arr.slice);
 console.log(arr);}
 
 }
